71e6dbe1bce190e0a17f2b247aa85e4254471062
Nope <dr.m.chadwick@gmail.com>
2015-10-12 17:27:13 +0100

    Implemented copy constructor and clone (deep copy).

--- imageProcessingTestBed/tournamentLayout.cs
+++ imageProcessingTestBed/tournamentLayout.cs
@@ -17,21 +17,71 @@ namespace imageProcessingTestBed
         public int Rounds { get; }
         public bool IncludeLosersBracket { get; }
         public List<int> MapsToWin { get; set; }
+        public List<int> LosersMapsToWin { get; set; }
 
         tournamentLayout(int nRounds, bool losersBracket = false)
         {
             Rounds = nRounds;
             IncludeLosersBracket = losersBracket;
-            UpperBracket = InitRounds(nRounds);
-            
+            UpperBracketLayout = InitRounds(nRounds);
 
+            //Set the final round to Bo5
+            MapsToWin.Add(3);
             //Earlier rounds to Bo3
-            for( int iMapsToWin = 0; iMapsToWin < nRounds - 1; iMapsToWin++)
+            for (int iMapsToWin = 1; iMapsToWin < nRounds - 1; iMapsToWin++)
             {
                 MapsToWin.Add(2);
             }
-            //Set the final round to Bo5
-            MapsToWin.Add(3);
+
+            if (IncludeLosersBracket)
+            {
+                InitLosersBracket();
+                LosersMapsToWin.Add(3);
+                for (int iMapsToWin = 1; iMapsToWin < nRounds - 2; iMapsToWin++)
+                {
+                    LosersMapsToWin.Add(2);
+                }
+
+            }
+
+
+        }
+
+        tournamentLayout(tournamentLayout inTourney)
+        {
+            this.IncludeLosersBracket = inTourney.IncludeLosersBracket;
+            this.LosersMapsToWin = inTourney.LosersMapsToWin;
+            this.LowerBracketLayout = inTourney.LowerBracketLayout;
+            this.MapsToWin = inTourney.MapsToWin;
+            this.Rounds = inTourney.Rounds;
+            this.UpperBracketLayout = inTourney.UpperBracketLayout;
+        }
+
+        public tournamentLayout Clone()
+        {
+            tournamentLayout newTournament = new tournamentLayout(Rounds, IncludeLosersBracket);
+            
+            for (int iRound = 0; iRound < Rounds; iRound++)
+            {
+                //Upper bracket clone
+                for (int iMatch = 0; iMatch < UpperBracketLayout.Count; iMatch++)
+                {
+                    newTournament.UpperBracketLayout[iRound][iMatch] = UpperBracketLayout[iRound][iMatch];
+                }
+                newTournament.MapsToWin[iRound] = MapsToWin[iRound];
+
+                //Lower bracket clone
+                if (IncludeLosersBracket && iRound < Rounds - 1)
+                {
+                    for (int iMatch = 0; iMatch < LowerBracketLayout.Count; iMatch++)
+                    {
+                        newTournament.LowerBracketLayout[iRound][iMatch] = LowerBracketLayout[iRound][iMatch];
+                    }
+                    newTournament.LosersMapsToWin[iRound] = LosersMapsToWin[iRound];
+                }
+            }
+
+            return newTournament;
         }
 
         private List<Match[]> InitRounds(int nRounds)
@@ -42,17 +92,17 @@ namespace imageProcessingTestBed
 
             //Generate a right to left bracket so finals are on the left.
             int nMatches = 1; //For keeping track of the 2 powers without math.pow
-            for(int iRound = 0; iRound < nRounds; iRound++)
+            for (int iRound = 0; iRound < nRounds; iRound++)
             {
-                for(int iMatch = 0; iMatch < nMatches; iMatch++)
+                for (int iMatch = 0; iMatch < nMatches; iMatch++)
                 {
                     tempLayout[iRound][iMatch] = new Match();
 
                     //Link to previous round!
-                    if(iRound > 0)
+                    if (iRound > 0)
                     {
                         //Integer division should ensure they link up correctly.
-                        tempLayout[iRound][iMatch].WinPathMatch = tempLayout[iRound-1][iMatch/2];
+                        tempLayout[iRound][iMatch].WinPathMatch = tempLayout[iRound - 1][iMatch / 2];
                     }
                 }
                 nMatches *= 2;
@@ -61,9 +111,9 @@ namespace imageProcessingTestBed
             return tempLayout;
         }
 
-        private List<Match[]> AddLosersBracket()
+        private List<Match[]> InitLosersBracket()
         {
-            if(Rounds <= 1)
+            if (Rounds <= 1)
             {
                 throw new System.IndexOutOfRangeException();
             }
@@ -85,5 +135,6 @@ namespace imageProcessingTestBed
             }
 
             return tempLayout;
+        }
     }
 }
