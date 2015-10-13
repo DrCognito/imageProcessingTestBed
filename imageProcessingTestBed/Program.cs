d75eaff8bfad104b630851d3630c19f0964bfd7b
Nope <dr.m.chadwick@gmail.com>
2015-10-13 01:41:49 +0100

    Cleaned up the location of some functions now in ProcessingTools.cs.

--- imageProcessingTestBed/Program.cs
+++ imageProcessingTestBed/Program.cs
@@ -171,8 +171,8 @@ namespace imageProcessingTestBed
             List<int> results = new List<int>();
             for (int row = 0; row < image1.Height; row++)
             {
-                BitArray test1 = testLine<TColor, TDepth>(image1, channelThresh, row);
-                BitArray test2 = testLine<TColor, TDepth>(image2, channelThresh, row);
+                BitArray test1 = ProcessingTools.testLine<TColor, TDepth>(image1, channelThresh, row);
+                BitArray test2 = ProcessingTools.testLine<TColor, TDepth>(image2, channelThresh, row);
 
                 BitArray resArray = test1.Xor(test2);
 
@@ -186,79 +186,9 @@ namespace imageProcessingTestBed
             }
             return results;
         }
-        static BitArray testLine<TColor, TDepth>(Emgu.CV.Mat imageLine, TDepth[] channelThresh, int row = 0)
-            where TColor : struct, IColor
-            where TDepth : IComparable<TDepth>, new()
-        {
-            if (imageLine.NumberOfChannels < channelThresh.Length)
-            { throw new ArgumentOutOfRangeException("channelThresh", "Array exceeds number of channels in image."); }
+        
 
-            Image<TColor, TDepth> tempImage = imageLine.ToImage<TColor, TDepth>();
-            BitArray outArray = new BitArray(imageLine.Width);
-
-            for (int pixel = 0; pixel < imageLine.Width; pixel++)
-            {
-                bool temp = true;
-                for (int channel = 0; channel < channelThresh.Length; channel++)
-                {
-                    temp &= channelThresh[channel].CompareTo(tempImage.Data[row, pixel, channel]) <= 0;
-                }
-                outArray[pixel] = temp;
-            }
-            return outArray;
-        }
-
-        static System.Drawing.Rectangle findTextEdge(Emgu.CV.Mat inImage)
-        {
-            Image<Emgu.CV.Structure.Gray, byte> tempImage = inImage.ToImage<Emgu.CV.Structure.Gray, Byte>();
-            byte whiteThresh = 150;
-
-            int rectX = 0;
-            int rectY = 0;
-            int rectWidth = 0;
-            int rectHeight = 0;
-
-
-            //Find the top left
-            for (int width = 0; width < tempImage.Width; width++)
-            {
-                bool done = false;
-                for (int height = 0; height < tempImage.Height; height++)
-                {
-                    if (tempImage.Data[height, width, 0] >= whiteThresh)
-                    {
-                        rectX = width;
-                        rectY = height;
-                        done = true;
-                        break;
-                    }
-
-                }
-                if (done) { break; }
-            }
-            //Starting from the top left, continue and find the last value
-            int botRightX = rectX;
-            int botRightY = rectY;
-
-            for (int width = rectX; width < tempImage.Width; width++)
-            {
-
-                for (int height = rectY; height < tempImage.Height; height++)
-                {
-                    if (tempImage.Data[height, width, 0] >= whiteThresh)
-                    {
-                        botRightX = width;
-                        botRightY = height;
-                    }
-                }
-
-            }
-
-            rectWidth = botRightX - rectX;
-            rectHeight = botRightY - rectY;
-
-            return new System.Drawing.Rectangle(rectX, rectY, rectWidth, rectHeight);
-        }
+        
 
         static Emgu.CV.Mat cannyEdges(Emgu.CV.Mat inImage)
         {
