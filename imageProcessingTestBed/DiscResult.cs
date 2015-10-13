8b1c52b6809d5b915e105851173b1a7d22f7e070
Nope <dr.m.chadwick@gmail.com>
2015-10-13 01:37:59 +0100

    Initial commit for discriminator results interface prototype.

--- imageProcessingTestBed/DiscResult.cs
+++ imageProcessingTestBed/DiscResult.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Emgu.CV;
 
 namespace imageProcessingTestBed
 {
-    interface DiscResult<returnT>
+    interface DiscResult
     {
-        returnT getResult();
+        float ProbabilityMatch(Mat inImage);
+        float CompareDiscriminators(DiscResult OtherDiscriminator, Mat inImage);
+        int MeasuredWithWidth();
+        int MeasuredWithHeight();
     }
 }
