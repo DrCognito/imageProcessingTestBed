5d213621ccfb6acdc0ed94b2cc1dacb9862d19eb
Nope <dr.m.chadwick@gmail.com>
2015-10-14 01:23:30 +0100

    Added uncertainty estimate, return probability with erf() first attempt.

--- imageProcessingTestBed/Discriminators/TextSize.cs
+++ imageProcessingTestBed/Discriminators/TextSize.cs
@@ -15,16 +15,27 @@ namespace imageProcessingTestBed.Discriminators
         private int ImageHeight = 0;
         public float RelativeWidth { get; }
         public float RelativeHeight { get; }
+        public float FractionalWidthUncertainty { get; }
+        public float FractionalHeightUncertainty { get; }
 
-        TextSize(Image inImage)
+        const double tightThreshold = 150.0;
+        const double looseThreshold = 120.0;
+        double denominator = Math.Sqrt(2);
+
+        TextSize(Image inImage, double looseThresh = looseThreshold, double tightThresh = tightThreshold)
         {
-            textRect = ProcessingTools.findTextEdge<Bgr, double>(inImage.loadedImage, new double[] { 120.0, 120.0, 120.0 });
+            textRect = ProcessingTools.findTextEdge<Bgr, double>(inImage.loadedImage, new double[] { tightThresh, tightThresh, tightThresh });
 
             ImageWidth = inImage.loadedImage.Width;
             RelativeWidth = (float)textRect.Width / (float)ImageWidth;
 
             ImageHeight = inImage.loadedImage.Height;
             RelativeHeight = (float)textRect.Height / (float)ImageHeight;
+
+            //Use looser rectangle to find the uncertainty.
+            System.Drawing.Rectangle looseRect = ProcessingTools.findTextEdge<Bgr, double>(inImage.loadedImage, new double[] { looseThresh, looseThresh, looseThresh });
+            FractionalWidthUncertainty = (looseRect.Width - textRect.Width) / (float)looseRect.Width;
+            FractionalHeightUncertainty = (looseRect.Height - textRect.Height) / (float)looseRect.Height;
         }
 
         public float CompareDiscriminators(IDiscResult OtherDiscriminator, Mat inImage)
@@ -44,10 +55,22 @@ namespace imageProcessingTestBed.Discriminators
 
         public float ProbabilityMatch(Mat inImage)
         {
-            System.Drawing.Rectangle ImageTextRect = ProcessingTools.findTextEdge<Bgr, double>(inImage, new double[] { 120.0, 120.0, 120.0 });
+            return ProbabilityMatch(inImage, tightThreshold);
+        }
+
+        public float ProbabilityMatch(Mat inImage, double tightThresh)
+        {
+            System.Drawing.Rectangle ImageTextRect = ProcessingTools.findTextEdge<Bgr, double>(inImage, new double[] { tightThresh, tightThresh, tightThresh });
 
             float ImageRelativeWidth = ImageTextRect.Width / (float)inImage.Width;
             float ImageRelativeHeight = ImageTextRect.Height / (float)inImage.Height;
+
+            float pixelStdError = inImage.Width * FractionalWidthUncertainty; //Should be evaluated more systematically based on the gradient i guess.
+
+            float StdDeviations = (ImageRelativeHeight - RelativeWidth) / pixelStdError;
+
+            return (float)Meta.Numerics.Functions.AdvancedMath.Erf(StdDeviations / denominator);
+
         }
     }
 }
